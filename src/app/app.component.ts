e2c37bdb4f3188ee4e4e36f46aafb9cb568213e2
Yesid-18 <gleyderyesid2017@gmail.com>
2023-08-22 17:02:49 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SharedService } from './services/shared.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appTareas';
+  title = 'apptareas';
+
+
+  constructor(private service:SharedService){}
+
+  notes:any=[];
+
+  listNotes(){
+      this.service.getNotes().subscribe((res) =>{
+        this.notes = res;
+      })
+  }
+
+  ngOnInit(){
+    this.listNotes();
+  }
+
+  addNotes(newNote:string){
+    this.service.addNotes(newNote).then((res) => {
+      console.log(res);
+      this.listNotes();
+    })
+  }
+
+  deleteNotes(id:string){
+    this.service.deleteNotes(id).then((res) => {
+      console.log(res);
+      this.listNotes();
+    })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBY3ucOB22ImybuDkUdSguxY39HTISUkdU",
+  authDomain: "app-tareas-7170b.firebaseapp.com",
+  projectId: "app-tareas-7170b",
+  storageBucket: "app-tareas-7170b.appspot.com",
+  messagingSenderId: "928390913997",
+  appId: "1:928390913997:web:a972bb81770400d3246e84"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp (firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
